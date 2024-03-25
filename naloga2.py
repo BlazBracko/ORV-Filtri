2f4fc5d4b0302da889271839a40d0fb300f30d3f
blazbracko <blaz.bracko03@gmail.com>
2024-03-25 16:13:19 +0100

    implementirana-gaussovaKrivulja

--- naloga2.py
+++ naloga2.py
@@ -19,9 +19,27 @@ def convolution(image, kernel):
 
     return output
 
-def filtriraj_z_gaussovim_jedrom(slika,sigma):
-    '''Filtrira sliko z Gaussovim jedrom..'''
-    pass
+def filter_with_gaussian_kernel(image, sigma):
+    kernel_size = int(2 * np.ceil(2 * sigma) + 1)
+
+    x = np.arange(-kernel_size // 2, kernel_size // 2 + 1)
+    y = np.arange(-kernel_size // 2, kernel_size // 2 + 1)
+    xx, yy = np.meshgrid(x, y)
+
+    gaussian_kernel = np.exp(-(xx ** 2 + yy ** 2) / (2 * sigma ** 2))
+    gaussian_kernel /= (2 * np.pi * sigma ** 2)
+    gaussian_kernel /= gaussian_kernel.sum()
+
+    # Padding to handle edges
+    padded_image = np.pad(image, [(kernel_size//2, kernel_size//2), (kernel_size//2, kernel_size//2)], mode='constant')
+
+    # Convolution
+    filtered_image = np.zeros_like(image)
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            filtered_image[i, j] = np.sum(padded_image[i:i+kernel_size, j:j+kernel_size] * gaussian_kernel)
+
+    return filtered_image
 
 def filtriraj_sobel_smer(slika):
     '''Filtrira sliko z Sobelovim jedrom in označi gradiente v orignalni sliki glede na ustrezen pogoj.'''
