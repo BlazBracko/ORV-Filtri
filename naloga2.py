a6ff80259422368da8e9871f683bbbb535092d29
blazbracko <blaz.bracko03@gmail.com>
2024-03-25 16:10:57 +0100

    implementirana-konvolucija

--- naloga2.py
+++ naloga2.py
@@ -1,10 +1,23 @@
 import cv2 as cv
 import numpy as np
 
-def konvolucija(slika, jedro):
-    '''Izvede konvolucijo nad sliko. Brez uporabe funkcije cv.filter2D, ali katerekoli druge funkcije, ki izvaja konvolucijo.
-    Funkcijo implementirajte sami z uporabo zank oz. vektorskega računanja.'''
-    pass
+def convolution(image, kernel):
+    height, width = image.shape
+    kernel_height, kernel_width = kernel.shape
+
+    output = null
+
+    padding_v = kernel_height // 2
+    padding_s = kernel_width // 2
+
+    padded_image = np.pad(image, ((padding_v, padding_v), (padding_s, padding_s)), mode='constant', constant_values=0)
+
+    for x in range(height):
+        for y in range(width):
+            patch = padded_image[x:x + kernel_height, y:y + kernel_width]
+            output[x, y] = np.sum(patch * kernel)
+
+    return output
 
 def filtriraj_z_gaussovim_jedrom(slika,sigma):
     '''Filtrira sliko z Gaussovim jedrom..'''
